5822d33b7a02898fdc10f809fc4dc2b4fc9a7d2b
matronix101 <chenmc@dukes.jmu.edu>
2023-01-27 12:14:32 -0500

    Lists assignsmes

--- Module 2/3 - Practice/2.3.0 - List Assignment/List_Assignment.py
+++ Module 2/3 - Practice/2.3.0 - List Assignment/List_Assignment.py
@@ -3,11 +3,11 @@
 
 # First, create a list with the values
 # 5, 10, 15, and 20, in that order:
-fives_list = None
+fives_list = [5, 10, 15, 20]
 
 # Now, store the last value of the list in
 # the variable tail:
-tail = None
+tail = fives_list[-1]
 
 # Here is another list
 new_list = [1, 2, 3, 4, 5]
@@ -15,16 +15,20 @@ new_list = [1, 2, 3, 4, 5]
 # Insert a 0 to the front of the list, then
 # append a 6 to the end of the list.
 # Hint: Should be two lines of code
-
+new_list.insert(0, 0)
+new_list.append (6)
+print(new_list)
 
 # Here is the another list:
 slicing_list = ["Luke", "Leia", "Han", "Vader", "R2D2"]
 
 # Store the first two elements in this new list:
-first_two = None
-
+first_two = slicing_list[:2]
+print(first_two)
 # Store the last three elements in this list:
-last_three = None
+last_three = slicing_list[-3:]
+print(last_three)
 
 # Lastly, copy the whole list into this list:
-slicing_copy = None
+slicing_copy = slicing_list
+print(slicing_copy)
